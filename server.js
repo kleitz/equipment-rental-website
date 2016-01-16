c0d36efcc01ab269f5403aa9c169307efc79694d
remon <remonasebi@gmail.com>
2016-01-16 16:49:06 +0000

    Added node server with livereload

---
+++ server.js
@@ -0,0 +1,19 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+var livereload = require('livereload');
+
+// Serve up public/ftp folder
+var serve = serveStatic('app', {'index': ['index.html', 'index.htm']})
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+var live = livereload.createServer();
+live.watch(__dirname + "/app");
+
+// Listen
+server.listen(80)
